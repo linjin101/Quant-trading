ff286ebe287a3e653c1aafd4e16956991fe873a2
linjin101 <53154641@qq.com>
2017-12-04 09:32:52 +0800

    修改注释和提示用语

--- 金融资料/船长/kaili.py
+++ 金融资料/船长/kaili.py
@@ -4,10 +4,16 @@ from __future__ import division
 import sys
 
 #赢可以赢多少？例如：1
+mystr="（赢可以赢多少？例如：10）=>"
+print mystr.decode('utf-8')
 y=input("win:")
 #输会输多少？例如：1
+mystr="（输可以输多少？例如：5 ）=>"
+print mystr.decode('utf-8')
 s=input("lose:")
 #胜率例如：0.5
+mystr="（胜率例如：0.5）=>"
+print mystr.decode('utf-8')
 p=input("p:")
 #y=15
 #s=7
@@ -25,5 +31,5 @@ f=(b*p-q)/b
 mystr="仓位:"
 print mystr.decode('utf-8'),f,"%"
 
-mystr="仓位数:"
-print mystr.decode('utf-8'),f*100
+mystr="仓位成数(总共10成):"
+print mystr.decode('utf-8'),f*10
