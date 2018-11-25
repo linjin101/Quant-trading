a3917bbc114a46a02274ebd75ebee268db8d7fa9
linjin101 <53154641@qq.com>
2018-11-25 22:51:03 +0800

    复合查询跟mongodb命令行几乎一样

--- python/testmongo.py
+++ python/testmongo.py
@@ -6,5 +6,5 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["test"]
 mycol = mydb["stocktemp2"]
 
-for x in mycol.find():
+for x in mycol.find({ "$and": [ { "GN": { "$eq": "5G" } }, { "GN": { "$eq": "军工" } }, { "GN": { "$eq": "创投" } } ] }):
 	print(x)
