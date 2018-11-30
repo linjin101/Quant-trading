d760300d16a9fcd382692ae2c194bac8c5c424a4
linjin101 <53154641@qq.com>
2018-11-30 18:13:11 +0800

    传送门采集微信历史文章

---
+++ php/stock/caiji/stockgd.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * 同花顺股票概念数据采集
+ */
+set_time_limit(0);   // 设置脚本最大执行时间 为0 永不过期
+
+include_once '../vendor/autoload.php'; //加载composer
+include_once '../common/config.php';
+include_once '../common/mongodb.php'; //加载数据库操作类 
+include_once '../common/mongodbStock.php'; //加载股票专用数据操作类 
+include_once '../common/common.php';
+
+use QL\QueryList;
+
+// $options指定选择列['projection' => ['code' => 1,'name'=> 1,'_id' => 0]] ,股票列表获取
+//$rowlist = $MongoDBOP->mongoSearch('test.stocklist', [], ['projection' => ['code' => 1,'name'=> 1,'_id' => 0]]);
+//检查没有采集成功的code和gn为空的重新采集
+$m = new MongoDBStock();
+$rowlist = $m->mongoSearch('test.stocklist', ['code' => '300343'], ['projection' => ['code' => 1, 'name' => 1, '_id' => 0]]); // ['code'=>'300343']
+
+$i = 1;
+foreach ($rowlist as $stockCode) {
+    //$url_page = 'http://vip.stock.finance.sina.com.cn/corp/go.php/vCI_CirculateStockHolder/stockid/' . $stockCode['code'] . '.phtml'; // $stockCode['code']
+
+    $url_page = 'http://chuansong.me/n/371498951536';
+    $string = QueryList::get($url_page)->removeHead()->getHtml();//->encoding('UTF-8', 'GB2312')
+    //解析常规概念
+//    $table = QueryList::html($string);
+//    $tableRows = $table->texts();
+//    $arrGN = $tableRows->all();
+    echo $i . "\r\n";
+    print_r( $string );
+//    $arrStockGN = array('code' => $stockCode['code'], 'name' => $stockCode['name'], 'GN' => $arrGN);
+//    $m->mongoInsert("test.stockgd", $arrStockGN); // date("Y-m-d")
+    /**
+     * 释放资源，销毁内存占用。在涉及到循环采集大量网页的场景下，这个方法是很有用的。
+     * 注意：此方法并不是销毁QueryList对象，只是销毁phpQuery Document占用的内存，
+     * 所以调用此方法后，原先设置过HTML的QueryList对象都会丢失设置的HTML，
+     * 需要重新调用html或者get方法设置HTML.
+     */
+    $table->destruct();
+//    if ($i % 3 == 0) {
+//        sleep(rand(1, 3));
+//    }
+    /*     * *************************************************************************
+     * Debug
+     * ************************************************************************* */
+
+
+    $i++;
+}
+/*
+  概念数组多条件查询db.getCollection('stocktemp2').find( { $and: [ { GN: { $eq: '5G' } }, { GN: { $eq: '军工' }}] } )
+  查询参数输入：[ '$and'=> [ [ 'GN'=> [ '$eq'=>'上海国资改革' ]], [ 'GN'=> [ '$eq'=>'上海自贸区' ]]  ] ]
+cd D:\dev\Quant-trading\php\stock\caiji
+D:
+D:\dev\phpStudy\PHPTutorial\php\php-7.2.1-nts\php stockgn.php
+ * 
+ * 
+cd E:\git\Quant-trading\php\stock\caiji\
+E:
+G:\phpStudy\PHPTutorial\php\php-7.2.1-nts\php stockgn.php
+ */
